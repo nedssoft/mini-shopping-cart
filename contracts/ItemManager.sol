e1b0f20a9858673a8cf2149afc445bb6fc6b982f
Chinedu Orie <nedsoftdeveloper@gmail.com>
2021-12-18 22:08:02 +0000

    create item manager contract

---
+++ contracts/ItemManager.sol
@@ -0,0 +1,46 @@
+pragma solidity 0.8.10;
+
+import "./Item.sol";
+import "./Ownable.sol";
+
+contract ItemManager is Ownable {
+
+    enum SupplyChainSteps{Created, Paid, Delivered}
+
+    struct S_Item {
+        Item _item;
+        ItemManager.SupplyChainSteps _step;
+        string _identifier;
+        address _address;
+    }
+    mapping(uint => S_Item) public items;
+    uint index;
+
+    event SupplyChainStep(uint _itemIndex, uint _step, address _address);
+
+    function createItem(string memory _identifier, uint _priceInWei) public onlyOwner {
+        Item item = new Item(this, _priceInWei, index);
+        address _address = address(item);
+        items[index]._item = item;
+        items[index]._step = SupplyChainSteps.Created;
+        items[index]._identifier = _identifier;
+        items[index]._address = _address;
+        emit SupplyChainStep(index, uint(items[index]._step), _address);
+        index++;
+    }
+
+     function triggerPayment(uint _index) public payable {
+        Item item = items[_index]._item;
+        require(address(item) == msg.sender, "Only items are allowed to update themselves");
+        require(item.priceInWei() == msg.value, "Not fully paid yet");
+        require(items[_index]._step == SupplyChainSteps.Created, "Item is further in the supply chain");
+        items[_index]._step = SupplyChainSteps.Paid;
+        emit SupplyChainStep(_index, uint(items[_index]._step), address(item));
+    }
+
+    function triggerDelivery(uint _index) public onlyOwner {
+        require(items[_index]._step == SupplyChainSteps.Paid, "Item is further in the supply chain");
+        items[_index]._step = SupplyChainSteps.Delivered;
+        emit SupplyChainStep(_index, uint(items[_index]._step), address(items[_index]._item));
+    }
+}
